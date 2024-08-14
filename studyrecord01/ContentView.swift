c9126cf9795e1338e08ca3f5b4b5a592c676109d
lakisis77 <lakisis7@gmail.com>
2024-08-14 17:14:47 +0900

    Initial Commit

---
+++ studyrecord01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  studyrecord01
+//
+//  Created by Min Lee on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ studyrecord01/studyrecord01App.swift
@@ -0,0 +1,17 @@
+//
+//  studyrecord01App.swift
+//  studyrecord01
+//
+//  Created by Min Lee on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct studyrecord01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
